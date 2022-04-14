74c6aff5b3326f88504f52ff1cc8917b795e6e3e
KaplanCoder <kemalselcukkaplan@outlook.com>
2022-04-14 15:43:31 +0300

    Location Mover coordinates movement changed

---
+++ LocationMover.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class Movement(str,Enum):
+    Left = "a"
+    Right = "d"
+    Down = "s"
+    Up = "w"
+
+def move(xCoordinate, yCoordinate, moveTypeString):
+    """
+    It takes  x-y coordinates and returns new coordinates
+    based on the move type.
+    :param xCoordinate:
+    :param yCoordinate:
+    :param moveTypeString:
+    :return:
+    """
+    if (moveTypeString == Movement.Left):
+        return (xCoordinate - 1, yCoordinate)
+
+    elif (moveTypeString == Movement.Right):
+        return (xCoordinate + 1, yCoordinate)
+
+    elif (moveTypeString == Movement.Down):
+        return (xCoordinate, yCoordinate + 1)
+
+    elif (moveTypeString == Movement.Up):
+        return (xCoordinate, yCoordinate - 1)
+    else:
+        raise Exception("Move type is not valid!")
