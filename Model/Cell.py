b85b149c48b29935bc3f9f43644bb860c7cec877
KaplanCoder <kemalselcukkaplan@outlook.com>
2022-04-17 14:20:21 +0300

    cell class is created to store the creature and manage its view

---
+++ Model/Cell.py
@@ -0,0 +1,25 @@
+from Creature import Creature
+
+class Cell:
+
+
+    def __init__(self, creature = None):
+        self.__creature=creature
+        self.__isViewAllowed=False
+
+    def setCreature(self,newCreature):
+        self.__creature=newCreature
+
+    def getCreature(self):
+        return self.__creature
+
+    def makeVisible(self):
+        self.__isViewAllowed=True
+
+    def makeInvisible(self):
+        self.__isViewAllowed=False
+
+    def __str__(self):
+        stringFormat="Safe" if (self.__creature is None) else str(self.__creature)
+        return stringFormat if self.__isViewAllowed else "Unknown"
+
