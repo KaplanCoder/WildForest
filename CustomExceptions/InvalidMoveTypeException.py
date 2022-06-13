9082c67addde34fe9ef845bde718554954b9e5b2
KaplanCoder <kemalselcukkaplan@outlook.com>
2022-06-13 22:46:57 +0300

    Invalid Move Type exception created

---
+++ CustomExceptions/InvalidMoveTypeException.py
@@ -0,0 +1,16 @@
+
+
+class InvalidMoveTypeException(Exception):
+
+    def __init__(self, *args):
+        self.__errorMessage = ""
+        if (args):
+            self.__errorMessage = args[0]
+
+    def setErrorMessage(self, newErrorMessage):
+        self.__errorMessage = newErrorMessage
+
+    def __str__(self):
+        if (len(self.__errorMessage) != 0):
+            return self.__errorMessage
+        return "Invalid Move Type error occured"
