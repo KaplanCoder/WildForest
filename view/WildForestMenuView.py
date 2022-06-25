d6e3fd5985603b94295055ac5d50409522103180
KaplanCoder <kemalselcukkaplan@outlook.com>
2022-06-25 16:43:09 +0300

    get move from the user method is changed.

--- view/WildForestMenuView.py
+++ view/WildForestMenuView.py
@@ -91,16 +91,18 @@ class WildForestMenuView:
 
     @classmethod
     def getMoveFromTheUser(self):
-        while (True):
+        isInputValid=False
+        while not (isInputValid):
             print("What is your move?")
             direction=input("Choose right (d), left (a), up (w) or down (s) : ")
             print() # added for new line
             if (isinstance(direction,str)):
                direction=direction.lower()
                enumList=[movementKey.value for movementKey in Movement]
-               if (direction in enumList):
-                    break # everything is ok
-            print("There is no such direction! Choose right (d), left (a), up (w) or down (s).")
+               if not (direction in enumList):
+                    print("There is no such direction! Choose right (d), left (a), up (w) or down (s).")
+               else:
+                   isInputValid=True
         return direction
 
 """ Testing
